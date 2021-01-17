410d73630da5d74771476740594c20e87ebab255
Grantjos12 <grantjoslyn@gmail.com>
2021-01-17 17:11:57 -0600

    changed date on Ui

--- ui.js
+++ ui.js
@@ -3,6 +3,8 @@ class UI {
     this.profile = document.getElementById("profile");
   }
   showProfile(user) {
+    let userDate = user.created_at;
+    let date = userDate.slice(0, 10);
     this.profile.innerHTML = `<div class="card card-body mb-3">
         <div class="row">
           <div class="col-md-3">
@@ -18,10 +20,10 @@ class UI {
             <ul class="list-group">
             <li class="list-group-item text-center text-primary h4 "> ${user.name}</li>
             <li class="list-group-item text-primary text-center"> ${user.bio}</li>
-              <li class="list-group-item">Company: ${user.company}</li>
+              <li class="list-group-item">Company:<span class="text-primary"> ${user.company}</span></li>
               <li class="list-group-item">Website/Blog: <a href="https://${user.blog}" target="_blank">${user.blog}</a></li>
-              <li class="list-group-item">Location: ${user.location}</li>
-              <li class="list-group-item">Member Since: ${user.created_at}</li>
+              <li class="list-group-item">Location: <span class="text-primary">${user.location}</span></li>
+              <li class="list-group-item">Member Since: <span class="text-primary">${date}</span></li>
             </ul>
           </div>
         </div>
